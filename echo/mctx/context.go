5fc6fe0751e14c53b70d3a2b283f757e2e69ba23
tamnt <milesofyou@gmail.com>
2019-12-27 17:33:16 +0700

    SAVEPOINT

--- echo/mctx/context.go
+++ echo/mctx/context.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -99,10 +100,30 @@ func ConvertURL(i interface{}) (values url.Values) {
 	if i == nil {
 		return
 	}
+	values = url.Values{}
 	iVal := reflect.ValueOf(i).Elem()
 	typ := iVal.Type()
 	for i := 0; i < iVal.NumField(); i++ {
-		values.Set(typ.Field(i).Name, fmt.Sprint(iVal.Field(i)))
+		f := iVal.Field(i)
+		tag := typ.Field(i).Tag.Get("tagname")
+		if tag != "" {
+			var v string
+			switch f.Interface().(type) {
+			case int, int8, int16, int32, int64:
+				v = strconv.FormatInt(f.Int(), 10)
+			case uint, uint8, uint16, uint32, uint64:
+				v = strconv.FormatUint(f.Uint(), 10)
+			case float32:
+				v = strconv.FormatFloat(f.Float(), 'f', 4, 32)
+			case float64:
+				v = strconv.FormatFloat(f.Float(), 'f', 4, 64)
+			case []byte:
+				v = string(f.Bytes())
+			case string:
+				v = f.String()
+			}
+			values.Set(typ.Field(i).Name, v)
+		}
 	}
 	return
 }
